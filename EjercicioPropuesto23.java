8bcf4f8577775542064e60af88e112422ba5ebb5
Jhony Alpala <jhoniher95@gmail.com>
2023-09-08 01:20:18 -0500

    Finalizado

--- EjercicioPropuesto23.java
+++ EjercicioPropuesto23.java
@@ -10,8 +10,8 @@ public class EjercicioPropuesto23 {
         double B=entrada.nextDouble();
         System.out.println("Ingrese el valor de C: ");
         double C=entrada.nextDouble();
-        double x1=(-1*B+Math.sqrt(Math.pow(B,2)-(4*A*C)))/(2*A);
-        double x2=(-1*B-Math.sqrt(Math.pow(B,2)-(4*A*C)))/(2*A);
+        double x1=(-1*B+Math.sqrt(Math.pow(B,2)-(4*A*C)))/(2*A); //aplicando la formula general
+        double x2=(-1*B-Math.sqrt(Math.pow(B,2)-(4*A*C)))/(2*A); //aplicando la formula general
         System.out.println("Las soluciones de la ecuacion son: \nX1="+x1+", X2="+x2);
 
 
